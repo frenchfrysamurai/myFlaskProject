69cfd6e25d847ac37d148d9e1139f0fa72c48c18
4P <frenchfrysamurai@gmail.com>
2024-06-18 14:10:03 -0500

    _

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -6,17 +6,18 @@ from werkzeug.exceptions import abort
 from flaskr.auth import login_required
 from flaskr.db import get_db
 
-bp = Blueprint('blog', __name__)
+bp = Blueprint('workout', __name__)
 
 @bp.route('/')
+@login_required
 def index():
     db = get_db()
-    posts = db.execute(
-            'SELECT p.id, title, body, created, author_id, username'
-            ' FROM post p JOIN user u ON p.author_id = u.id'
+    workouts = db.execute(
+            'SELECT w.id, w.date, username'
+            ' FROM workout p JOIN user u ON p.author_id = u.id'
             ' ORDER BY created DESC'
     ).fetchall()
-    return render_template('blog/index.html', posts=posts)
+    return render_template('workout/index.html', workouts=workouts)
 
 @bp.route('/create', methods=('GET', 'POST'))
 @login_required
